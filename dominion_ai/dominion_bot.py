02cd007d1b5d1e99d7df177738f5b7e96fb9e8a2
kiwidamien <djmartin@ucdavis.edu>
2019-11-11 22:06:29 -0800

    have fire and CLI working

--- dominion_ai/dominion_bot.py
+++ dominion_ai/dominion_bot.py
@@ -1,12 +1,27 @@
 from dominion_ai.game import make_std_game, make_prosperity_game
 from dominion_ai.green_gold import GreenGoldPlayer
+import fire
 
-
-def main():
-    print("Hello from dominion_bot")
-    my_game = make_std_game(2)
-    my_game = make_prosperity_game(2)
-    gg_player = GreenGoldPlayer(my_game)
+def start_the_game(n_players=2, prosperity=False,
+                   has_curses=False, is_silent=False, late_game=3):
+    greeting = f"""
+    Welcome to Dominion bot:
+      Configuration:
+      --------------
+      Players:       {n_players:2d}
+      Game Type:     {'prosperity' if prosperity else 'standard'}
+      Voice:         {not is_silent}
+      Using curses?  {'yes' if has_curses else 'no'}
+      "Late game"    {late_game} province in bots hand
+      
+      """
+    print(greeting)
+    if prosperity:
+        game_fn = make_prosperity_game
+    else:
+        game_fn = make_std_game
+    my_game = game_fn(n_players, is_silent)
+    gg_player = GreenGoldPlayer(my_game, late_game)
 
     while True:
         _ = input("\nPress Enter to take a turn...\n")
@@ -14,5 +29,5 @@ def main():
         gg_player.play_hand()
         gg_player.draw_hand()
 
-if __name__ == '__main__':
-    main()
+def main():
+    fire.Fire(start_the_game)
